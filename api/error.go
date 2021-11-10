3a9ef59dcce4126f1f527f8e380e31003dafd465
bCoder778 <bCoder778@gmail.com>
2021-11-10 13:38:55 +0800

    Update error.go

--- api/error.go
+++ api/error.go
@@ -7,9 +7,12 @@ import (
 const (
 	ERROR_UNKNOWN                     = 1
 	Error_Addr_Invalid                = 2
-	ERROR_PARAM                       = 0x00010101
-)
 
+	ERROR_REQUEST_UNAUTHPRIZED        = 0x00000191
+	ERROR_REQUEST_NODFOUND            = 0x00000194
+	ERROR_PARAM                       = 0x00010101
+	ERROR_FORM_INIT_FAILED            = 0x00050101
+)
 
 type Error struct {
 	Code    int
